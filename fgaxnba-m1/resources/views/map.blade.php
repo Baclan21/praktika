6fd2edcfbb1fd0ccd5f529fd7a4b7dc4d210bce0
Baclan21 <astdvyrn@wsr.ru>
2023-02-25 00:33:35 -0800

    first commit

---
+++ fgaxnba-m1/resources/views/map.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<center>
+<div class="card">
+    <div class="card-header"><img src="../resources/views/img/map.jpg"></div>
+
+    <div class="card-body">
+        <a> Наш магазин расположен на улице северо-восточной 34.</a>
+        <a> Контактные данные для связи: 8952358541, truegames@mail.ru.</a>
+            </div>
+    </div>
+@endsection
